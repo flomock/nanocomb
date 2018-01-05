06b5c6aa77635d09a43a6616e0ab1f65ae8e7436
flomock <florian_mock91@web.de>
2018-01-05 18:07:20 +0100

    finished: filtering of samples by definition of clades

--- nanocomb/tree.py
+++ nanocomb/tree.py
@@ -1,11 +1,11 @@
 # stackoverflow, 26146623
 import csv
 from collections import defaultdict
-# import itertools
 from pprint import pprint
 import os
 from pymongo import MongoClient
-
+import pandas as pd
+import numpy as np
 
 def new_tree(): return defaultdict(new_tree)
 
@@ -102,15 +102,102 @@ def add_all_leafs(file, collection):
 
 
 
-def get_db(mongoclient="localhost:27017", db_name="testDB", collection_name="testCell"):
+def get_db(mongoclient="localhost:27017", db_name="testDB", collection_name="testCell2"):
     client = MongoClient(mongoclient)
     db = client[db_name]
     collection = db.get_collection(collection_name)
     return collection
 
+def get_samples(virus_file, host_file, collection, host_clade="all", virus_clade="all"):
+    """
+    get samples which fulfill the clade conditions
+    :param virus_file: dict tree for viruses
+    :param host_file: dict tree for hosts
+    :param collection: the database
+    :param host_clade: which host clade should be used
+    :param virus_clade: which virus clade should be used
+    :return: samples as pandas array
+    """
+    #zugriff auf daten durch angabe von Host und Clade
+    def get_leaves(file):
+        """
+        go to leafs of tree and put together in array
+        :param file: dict tree representation
+        :return: Array[Ids]
+        """
+        leave = np.array([])
+        for k in iter(list(file)):
+
+            if len(list(file[k])) > 0:
+                leave = np.append(leave,get_leaves(file[k]))
+            else:
+                leave = np.append(leave,k)
+
+        return leave
+
+    def get_ids(file, filter):
+        """
+        get ids for clade
+        :param file: tree of hosts or viruses
+        :param filter: clade of interest
+        :return: all leaves of interest
+        """
+        for k in iter(file.keys()):
+            if k == filter:
+                virus_leaves = get_leaves(file[k])
+                return virus_leaves
+            else:
+                if len(file[k].keys()) > 0:
+                    found = get_ids(file[k],filter)
+                    if len(found) > 0:
+                        return found
+        return []
+
+    if virus_clade == "all":
+        virus_leaves = get_leaves(virus_file)
+    else:
+        virus_leaves = get_ids(virus_file, virus_clade)
+
+    if host_clade == "all": #TODO delete hard coded example
+        host_leaves = np.array(["ae02c884-187c-492a-a1be-880df33c3f51", "71057421-e62a-4633-b3ba-f2348f77c4bc"])
+        # host_leaves = get_leaves(host_file)
+    else:
+        # host_leaves = get_ids(host_file,host)
+        host_leaves = np.array(["ae02c884-187c-492a-a1be-880df33c3f51","71057421-e62a-4633-b3ba-f2348f77c4bc"])
+    assert len(virus_leaves) > 0, "no valid virus clade"
+    assert len(host_leaves) > 0, "no valid host clade"
+
+    def compare_ids(virus,host):
+        ids = set(virus).intersection(host)
+        return ids
+
+    ids = compare_ids(virus_leaves,host_leaves)
+
+    def get_table_samples(ids):
+        """
+        parse ids to usable sample table
+        :param ids: ids of interest
+        :return: pandas dataframe with id, host, seq and parent of samples
+        """
+        l=[]
+        for id in ids:
+            for element in collection.find({"_id": id}):
+                l.append(element)
+        gen = ((i['_id'], i['host'], i['seq'], i['parent']) for i in l)
+        df = pd.DataFrame.from_records(gen, columns=['id', 'host', 'seq', 'parent'])
+        print(df)
+        # test = pd.DataFrame()
+        # for h in HOSTS:
+        #     df_host = df[df.host == h]
+        #     test_sub = df_host.sample(frac=SIZE, random_state=SEED)
+        #     test = test.append(test_sub)
+
+    get_table_samples(ids)
+
+
 cwd = os.getcwd()
 collection = get_db()
-file = csv2dict(cwd + "/examples/small.csv")
-add_all_leafs(file,collection)
-print(tree_to_newick(file))
-# add_all_leafs(tree,collection)
+virus_tree = csv2dict(cwd + "/examples/small.csv")
+add_all_leafs(virus_tree, collection)
+print(tree_to_newick(virus_tree))
+samples = get_samples(virus_tree, None, collection, virus_clade="all")
